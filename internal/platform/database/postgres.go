d4c1b2c6fbdc45cd5bbfe5d4dbce95608feae3e8
kittiphops <kittiphopsompuech2@gmail.com>
2025-10-31 16:21:54 +0700

    feat:add migrate func

--- internal/platform/database/postgres.go
+++ internal/platform/database/postgres.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/stdlib"
+	"github.com/kittiphop/zombie_board_game_back/internal/domain/model"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -46,5 +47,16 @@ func InitializePostgres(dsn string) (*gorm.DB, error) {
 
 // ===== Database Migration =====
 func Migrate(db *gorm.DB) error {
+	log.Println("🚀 Running database migrations...")
+
+	if err := db.AutoMigrate(
+		&model.User{},
+		&model.Room{},
+	); err != nil {
+		log.Println("❌ Migration failed:", err)
+		return err
+	}
+
+	log.Println("✅ Migration completed!")
 	return nil
 }
